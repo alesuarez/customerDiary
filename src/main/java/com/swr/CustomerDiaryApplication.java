153401b5188412618cd3cd05a35386a2751d672d
Alejandro Suarez <sas.tuc@gmail.com>
2017-03-14 17:14:58 -0300

    jar

--- src/main/java/com/swr/CustomerDiaryApplication.java
+++ src/main/java/com/swr/CustomerDiaryApplication.java
@@ -2,18 +2,11 @@ package com.swr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CustomerDiaryApplication extends SpringBootServletInitializer {
+public class CustomerDiaryApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CustomerDiaryApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(CustomerDiaryApplication.class);
-    }
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(CustomerDiaryApplication.class, args);
+  }
 }
